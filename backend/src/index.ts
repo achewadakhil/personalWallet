5173a7a8e3caa50dc98decfb55f10bd9d8d2c691
Akhil <achewadakhil11@gmail.com>
2025-08-02 14:05:30 +0530

    Create multilple paths in a HD wallet(SOL & ETH)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,38 @@
 import { Keypair } from "@solana/web3.js";
-import { Buffer } from "buffer";
-import nacl from "tweetnacl";
+import * as bip39 from "bip39";
+import { derivePath } from "ed25519-hd-key";
+import { HDNodeWallet , Wallet} from "ethers"
+(async ()=>{
+    //generating the mnemonics
+    const mnemonics = bip39.generateMnemonic(128);
 
+    console.log(mnemonics);
 
-const keyPair = Keypair.generate();//genertion
-const publicKey = keyPair.publicKey;
+    //making seed from the mnemonic
+    const seed = await bip39.mnemonicToSeed(mnemonics);
 
-const privateKey = Buffer.from(keyPair.secretKey).toString("base64");
+    console.log("Seed : ",Buffer.from(seed).toString("base64"));
 
-console.log("PublicKey :",publicKey.toString());
-console.log("PrivateKey : ", privateKey);
+    //lets make a HD WALLET NOW no ethereum chain
 
-const message = "Hello, Solana!";
-const encodedMessage = new TextEncoder().encode(message)
+    //create a root HDNode from the seed it is a generic root not depended on any chain
+    const root = HDNodeWallet.fromSeed(seed);
 
-const dup = new TextEncoder().encode("x");
-const signature = nacl.sign.detached(encodedMessage , keyPair.secretKey);
+    //generating 3 accounts for sample
+    const ethereum = `m/44'/60'/0'/0/`;
+    console.log("Ethereum: ");
+    const ethPath = root.derivePath(ethereum+"0");
+    const ethChild = new Wallet(ethPath.privateKey);
+    console.log(ethChild.privateKey);
+    console.log(ethChild.address);
+    const solana = `m/44'/501'/0'/0'`;
+    console.log("Solana : ")
+    const derivedSeed = derivePath(solana,seed.toString("hex"));
 
+    const keypair = Keypair.fromSeed(derivedSeed.key);
 
-console.log("Signature :",Buffer.from(signature).toString("Base64"));
+    console.log(Buffer.from(keypair.secretKey).toString("base64"));
+    console.log("Public key : ",keypair.publicKey.toBase58())
 
-const isValid = nacl.sign.detached.verify(
-    dup,
-    signature,
-    publicKey.toBytes()
-)
-console.log(isValid);
+})();
 
